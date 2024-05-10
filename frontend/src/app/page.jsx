6d62557ee270bfdcc7181d615d41c622ded6fff6
Jhanvi-Khare <jhanvikhare26@gmail.com>
2024-05-10 13:13:24 +0530

    signup

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -57,7 +57,7 @@ const page = () => {
             {/* Buttons */}
             <div className="text-center">
               <a
-                className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-black text-sm font-medium rounded-full focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-6 dark:focus:ring-offset-gray-800"
+                className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-white  text-sm font-medium rounded-full focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-6 dark:focus:ring-offset-gray-800"
                 href="#"
               >
                 Get started
@@ -89,137 +89,7 @@ const page = () => {
               src="https://png.pngtree.com/thumb_back/fh260/back_our/20190620/ourmid/pngtree-educational-institution-autumn-admission-poster-background-image_146770.jpg"
             />
           </div>
-      <>
-        {/* Icon Blocks */}
-        <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-          <div className="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-12">
-            {/* Icon Block */}
-            <div>
-              <svg
-                className="flex-shrink-0 size-9 text-gray-800 dark:text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <rect width={10} height={14} x={3} y={8} rx={2} />
-                <path d="M5 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-2.4" />
-                <path d="M8 18h.01" />
-              </svg>
-              <div className="bg-gradient-to-r from-gray-200 via-gray-50 to-transparent h-0.5 mt-6 dark:from-neutral-700 dark:via-neutral-900">
-                <div className="bg-gray-400 w-9 h-0.5 dark:bg-neutral-600" />
-              </div>
-              <div className="mt-5">
-                <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-                  Responsive
-                </h3>
-                <p className="mt-1 text-gray-600 dark:text-neutral-400">
-                  Responsive, and mobile-first project on the web
-                </p>
-              </div>
-            </div>
-            {/* End Icon Block */}
-            {/* Icon Block */}
-            <div>
-              <svg
-                className="flex-shrink-0 size-9 text-gray-800 dark:text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M20 7h-9" />
-                <path d="M14 17H5" />
-                <circle cx={17} cy={17} r={3} />
-                <circle cx={7} cy={7} r={3} />
-              </svg>
-              <div className="bg-gradient-to-r from-gray-200 via-gray-50 to-transparent h-0.5 mt-6 dark:from-neutral-700 dark:via-neutral-900">
-                <div className="bg-gray-400 w-9 h-0.5 dark:bg-neutral-600" />
-              </div>
-              <div className="mt-5">
-                <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-                  Customizable
-                </h3>
-                <p className="mt-1 text-gray-600 dark:text-neutral-400">
-                  Components are easily customized and extendable
-                </p>
-              </div>
-            </div>
-            {/* End Icon Block */}
-            {/* Icon Block */}
-            <div>
-              <svg
-                className="flex-shrink-0 size-9 text-gray-800 dark:text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
-                <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
-              </svg>
-              <div className="bg-gradient-to-r from-gray-200 via-gray-50 to-transparent h-0.5 mt-6 dark:from-neutral-700 dark:via-neutral-900">
-                <div className="bg-gray-400 w-9 h-0.5 dark:bg-neutral-600" />
-              </div>
-              <div className="mt-5">
-                <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-                  Documentation
-                </h3>
-                <p className="mt-1 text-gray-600 dark:text-neutral-400">
-                  Every component and plugin is well documented
-                </p>
-              </div>
-            </div>
-            {/* End Icon Block */}
-            {/* Icon Block */}
-            <div>
-              <svg
-                className="flex-shrink-0 size-9 text-gray-800 dark:text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M14 9a2 2 0 0 1-2 2H6l-4 4V4c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v5Z" />
-                <path d="M18 9h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1" />
-              </svg>
-              <div className="bg-gradient-to-r from-gray-200 via-gray-50 to-transparent h-0.5 mt-6 dark:from-neutral-700 dark:via-neutral-900">
-                <div className="bg-gray-400 w-9 h-0.5 dark:bg-neutral-600" />
-              </div>
-              <div className="mt-5">
-                <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-                  24/7 Support
-                </h3>
-                <p className="mt-1 text-gray-600 dark:text-neutral-400">
-                  Contact us 24 hours a day, 7 days a week
-                </p>
-              </div>
-            </div>
-            {/* End Icon Block */}
-          </div>
-        </div>
-        {/* End Icon Blocks */}
-      </>
+     
       <div>
         
       <section className="bg-white dark:bg-gray-900">
@@ -306,7 +176,7 @@ const page = () => {
             fill="#7629C8"
           />
         </svg>
-        <span className="sr-only">Kutty Home Page</span>
+        <span className="sr-only">Home Page</span>
       </a>
       <p className="my-4 text-xs leading-normal text-gray-600">
         Hosted in the EU 🇪🇺, with <strong>no user tracking</strong> scripts.
